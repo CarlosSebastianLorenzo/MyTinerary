74a9a08dbde4502e6af2bfb16d35173daca23b39
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-01-31 14:30:23 -0300

    Change link Images

--- src/pages/Mockups.jsx
+++ src/pages/Mockups.jsx
@@ -1,12 +1,24 @@
 const Mockups = () => {
     return (
         <main style={{flexDirection:"column", maxWidth:"1000px"}}>
-            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}} src="public/homeMock.png" alt="myTinerary Home Page"/>
-            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}} src="public/darkModeMock.png" alt="myTinerary Home Page Dark Mode"/>
-            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}} src="public/citiesSearchMock.png" alt="myTinerary Cities Search"/>
-            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}} src="public/responsiveMock.png" alt="myTinerary Responsive"/>
-            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}} src="public/loginSignOutMock.png" alt="myTinerary Log In and Sign Up"/>
-            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}} src="public/itinerariesMock.png" alt="myTinerary Interactive Itineraries"/>
+            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}}
+                src="https://raw.githubusercontent.com/CarlosSebastianLorenzo/MyTinerary/master/public/homeMock.png"
+                alt="myTinerary Home Page"/>
+            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}}
+                src="https://raw.githubusercontent.com/CarlosSebastianLorenzo/MyTinerary/master/public/darkModeMock.png"
+                alt="myTinerary Home Page Dark Mode"/>
+            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}}
+                src="https://raw.githubusercontent.com/CarlosSebastianLorenzo/MyTinerary/master/public/citiesSearchMock.png"
+                alt="myTinerary Cities Search"/>
+            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}}
+                src="https://raw.githubusercontent.com/CarlosSebastianLorenzo/MyTinerary/master/public/responsiveMock.png"
+                alt="myTinerary Responsive"/>
+            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}}
+                src="https://raw.githubusercontent.com/CarlosSebastianLorenzo/MyTinerary/master/public/loginSignOutMock.png"
+                alt="myTinerary Log In and Sign Up"/>
+            <img style={{width:"100%", objectFit:"contain", borderRadius:"25px"}}
+                src="https://raw.githubusercontent.com/CarlosSebastianLorenzo/MyTinerary/master/public/itinerariesMock.png"
+                alt="myTinerary Interactive Itineraries"/>
         </main>
         
     )
