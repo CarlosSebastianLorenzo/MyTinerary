2b80c668213570b04650e0e3479cc79366d12dcd
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-04-08 17:32:49 -0300

    Text Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,8 @@ export default function Hero() {
     return (
         <main>
             <section>
-                <h1>Find the <span className='acent'>perfect</span> destination</h1>
-                <p>Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</p>
+                <h1>Find <span className='acent'>your perfect </span> trip,</h1>
+                <h3>designed by insiders who know and love their cities!</h3>
                 <Link to='cities'>
                     <button>View More</button>
                 </Link>
