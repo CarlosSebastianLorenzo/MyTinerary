0333d3d0e62a5d5d441e5ad69705d03a61b90e4d
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-07-22 20:00:01 -0300

    CSS display main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
       <header>
-        <h3>My Tinerary</h3>
+        <h2>My Tinerary</h2>
         <nav>
           <ul>
             <li>
@@ -26,7 +24,7 @@ function App() {
           <p>Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</p>
           <button className="callToAction">View More</button>
         </section>
-        <img className="heroImg" src="" alt="" />
+        <img className="heroImg" src="https://img.itinari.com/pages/images/original/58e9ad24-584f-4321-9c5f-5a55e656ac0a-istock-518230906-copia.jpg?ch=DPR&dpr=2.625&w=1600&s=06821398aa6ac7b719cb5e37121240f0" alt="Brazil" />
       </main>
       <footer>
         <p className="copyright">Copyright 2022© Lorenzo Carlos Sebastian - All Rights Reserved</p>
