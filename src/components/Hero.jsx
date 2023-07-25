6cad7b2182eb0008946582abf82a3cb95087c442
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-07-25 20:00:01 -0300

    Create Hero Component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+    return (
+        <main>
+            <section>
+                <h1>Find the perfect destination</h1>
+                <p>Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</p>
+                <button>View More</button>
+            </section>
+            <img className="heroImg" src="https://img.itinari.com/pages/images/original/58e9ad24-584f-4321-9c5f-5a55e656ac0a-istock-518230906-copia.jpg?ch=DPR&dpr=2.625&w=1600&s=06821398aa6ac7b719cb5e37121240f0" alt="Brazil" />
+        </main>
+    )
+}
