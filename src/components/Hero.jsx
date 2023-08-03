aa1422ac5b4cdfb80b80dc0c8a6b7cf93bd3de86
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-08-03 20:00:01 -0300

    circles of Carousel and flex styles of Carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react"
-import Carrousel from "../layouts/Carrousel"
+// import { useEffect } from "react"
+import Carousel from "../layouts/Carousel"
 import Cities from "../Utils/citiesMockup"
 
 export default function Hero() {
@@ -11,12 +11,12 @@ export default function Hero() {
                 <p>Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</p>
                 <button>View More</button>
             </section>
-            <Carrousel>
-            {Cities.map((city,indexMap) => {
-                return <img className="heroImg" loading="eager" key={indexMap} src={city.photo} alt={city.city} />
-                }
-            )}
-            </Carrousel>
+                <Carousel>
+                {Cities.map((city,indexMap) => {
+                    return <img className="heroImg" loading="eager" key={indexMap} src={city.photo} alt={city.city} />
+                    }
+                )}
+                </Carousel>
         </main>
     )
 }
---
+++ src/layouts/Carousel.jsx
@@ -0,0 +1,49 @@
+import {useState} from 'react'
+import leftArrow from '/leftarrow.svg'
+import rigthArrow from '/rigtharrow.svg'
+import circle from '/circle.svg'
+import circleEmpty from '/circleEmpty.svg'
+import Icon from './Icon'
+
+export default function Carousel({children}) {
+    const elementsArray = children.map(child => child)
+    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedElement, setSelectedElement] = useState(elementsArray[0]);
+
+    const previous = () => {
+        const condition = selectedIndex > 0;
+        const nextIndex = condition ? selectedIndex - 1 : elementsArray.length - 1;
+        setSelectedElement(elementsArray[nextIndex]);
+        setSelectedIndex(nextIndex);
+    };
+
+    const next = () => {
+        const condition = selectedIndex < elementsArray.length - 1;
+        const nextIndex = condition ? selectedIndex + 1 : 0;
+        setSelectedElement(elementsArray[nextIndex]);
+        setSelectedIndex(nextIndex);
+    };
+
+    const select = (index) => {
+        setSelectedElement(elementsArray[index]);
+        setSelectedIndex(index);
+    }
+
+    return (
+        <div className='slide'>
+            <div className="contentSlide">
+                <Icon fn={previous} icon={leftArrow}/>
+                {selectedElement}
+                <Icon fn={next} icon={rigthArrow}/>
+            </div>
+            <div className='selectors'>
+                {elementsArray.map((child,indexmap) =>{
+                        let pepe
+                        indexmap === selectedIndex ? pepe = circle :  pepe = circleEmpty
+                        return <Icon key={indexmap} fn={()=>select(indexmap)} icon={pepe}/>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
