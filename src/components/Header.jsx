240477fab70dba72a14240c834110811a2a4de5e
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-08-04 20:00:01 -0300

    logo navBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 function Header() {
     return (
         <header>
-            <h2>My Tinerary</h2>
+            <div>
+                <img src="https://logomaker.designfreelogoonline.com/media/productdesigner/logo/resized/000931_plane_flying_logo_design_free_online_travel_logo_maker-06.png" alt="logo" />
+                <h2>My Tinerary</h2>
+            </div>
             <nav>
                 <ul>
                     <li>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ export default function Hero() {
     return (
         <main>
             <section>
+
                 <h1>Find the perfect destination</h1>
                 <p>Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</p>
                 <button>View More</button>
