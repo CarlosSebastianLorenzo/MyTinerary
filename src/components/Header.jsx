49c9bed2d11d2aa2fdee348592eb58511001717d
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-08-08 20:00:01 -0300

    styles buttons, anchors, link logo to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom"
 function Header() {
     return (
         <header>
-            <div>
+            <NavLink to="/">
                 <img src="https://logomaker.designfreelogoonline.com/media/productdesigner/logo/resized/000931_plane_flying_logo_design_free_online_travel_logo_maker-06.png" alt="logo" />
                 <h2>My Tinerary</h2>
-            </div>
+            </NavLink>
             <nav>
                 <ul>
                     <li>
