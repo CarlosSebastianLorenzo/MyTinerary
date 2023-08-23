4a2479821bc1f96fc5fcc0aae235fee378619e45
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-08-22 22:00:23 -0300

    Fetch, HTML and CSS in Details Page + Navigation to back

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,19 +1,48 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import UnderConstruction from "../components/UnderConstruction"
-import { useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
+import { apiURL } from "../Utils/apiURL"
+import personWalkingLuggage from "/personwalkingluggage.svg"
+
 
 const Details = () => {
     const params = useParams()
-    console.log(params.id)
+    const navigate = useNavigate()
+    let [infoCity, setInfoCity] = useState({})
 
     useEffect(() => {
         document.title = params.id + " - MyTinerary"
     },[])
 
+    useEffect(() => {
+        fetch(apiURL+"cities/"+params.id)
+        .then(response => response.json())
+        .then(data => setInfoCity(data.response))
+    },[])
+
     return (
+        <>
+            <div className="details">
+                <div style={{backgroundImage: `url(${infoCity.photo})`}}>
+                    <h1 className='acent'>{infoCity.city}</h1>
+                    <h2>{infoCity.country}</h2>
+                </div>
+                <span>
+                    <p>Population: <span className="acent">{infoCity.population}</span></p>
+                    <p>Fundation: <span className="acent">{infoCity.fundation}</span></p>
+                    <p>Featured Locations: <span className="acent">{infoCity.featuredLocation}</span></p>
+                </span>
+                <p>{infoCity.description}</p>
+                <button onClick={()=>navigate(-1)}>
+                    Go Back to Cities
+                    <img src={personWalkingLuggage} alt="personWalkingLuggage" />
+                </button>
         <UnderConstruction>
         
         </UnderConstruction>
+            </div>
+        </>
     )
 }
 
