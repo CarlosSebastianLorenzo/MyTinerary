3c8c3d618729ba1ef4eb6595b8004243002a3e58
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-08-26 14:30:23 -0300

    install react-icons, styles svg path fill on Hover, change icon in under Construction component

--- src/components/UnderConstruction.jsx
+++ src/components/UnderConstruction.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom"
-import personWalkingLuggage from "/personwalkingluggage.svg"
 import underconstruction from "/underconstruction.svg"
+import { FaPersonWalkingLuggage } from "react-icons/fa6";
+
 
 const UnderConstruction = () => {
     return (
@@ -9,7 +10,7 @@ const UnderConstruction = () => {
                 <h1>Page Under <span className="acent">Construction</span></h1>
                 <h3>Content will be available soon</h3>
                 <NavLink to="/"><button>Go Back Home 
-                <img src={personWalkingLuggage} alt="personWalkingLuggage" /></button></NavLink>
+                <FaPersonWalkingLuggage className="highlight"   /></button></NavLink>
         </section>
         <aside style={{minHeight: '10vh', height: '20vh'}}>
             <img style={{maxWidth: '90vw', maxHeight: '60vh'}} src={underconstruction} alt="workers construction house" />
